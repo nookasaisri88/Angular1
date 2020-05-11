0dc648aa9f7f9f17b1b79a1f0aa64090e854934a
N Sai Sri <nookasaisri88@gmail.com>
2020-05-11 18:34:40 -0400

    spa part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import {DishService} from './services/dish.service';
-
+import {PromotionService} from './services/promotion.service';
 import {AppRoutingModule} from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
@@ -43,7 +43,9 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
